939fca6edbbbf2aa0374ae0d2c1b8e5b6120bce1
BlaineParr <Blaine.parr@hotmail.com>
2015-01-22 15:34:09 -0500

    Created Project

---
+++ COMP1011-Lesson03/src/Project.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Blaine Parr
+ * Program Desctiption: Demo files for COMP1011 Lesson 03
+ * @version 0.1 - Created Project
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
